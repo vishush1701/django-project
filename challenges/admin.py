2f9df99d27e7d65c6a6da7286fb394c1c081dfa3
vishwanathHiremath <vishush1701@gmail.com>
2024-06-17 23:33:37 +0530

    admin panel configure readonly fields

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Challenge
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+   readonly_fields = ("months_challenge",)
+   prepopulated_fields ={"months_challenge":("month")}
+
+admin.site.register(Challenge)
--- challenges/models.py
+++ challenges/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Challenge(models.Model):
     month = models.CharField(max_length=10,null=False)
     challenge = models.CharField(max_length=25,null=False)
     is_active = models.BooleanField(default=False)
+    months_challenge = models.SlugField(max_length = 20,editable=False,blank=True,default="")
     managed = True
+    
+
+    def save(self,*args, **kwargs):
+        self.months_challenge = slugify(self.month+" challenge")
+        super().save(*args,**kwargs)
 
     def __str__(self) -> str:
-        return f"month:{self.month} challenge:{self.challenge} is_active:{self.is_active}"
+        return f"month:{self.month} challenge:{self.challenge} is_active:{self.is_active} months_challenge={self.months_challenge}"
